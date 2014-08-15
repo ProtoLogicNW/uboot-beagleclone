5afc5f095ae476ee4c4628864b3c7b714c3d4253
Chris Vondrachek <chris@protologicnw.com>
2014-08-14 22:58:35 -0700

    Updated scripts to be portable to other home directories Fixed gitignore; was omitting spl/Makefile Updated todo

--- buildUBoot.sh
+++ buildUBoot.sh
@@ -1,2 +1,1 @@
-#make -C /home/cvondrachek/ti-sdk-am335x-evm-07.00.00.00/board-support/u-boot-* CROSS_COMPILE=/home/cvondrachek/ti-sdk-am335x-evm-07.00.00.00/linux-devkit/sysroots/i686-arago-linux/usr/bin/arm-linux-gnueabihf- clean
-make -C /home/cvondrachek/ti-sdk-am335x-evm-07.00.00.00/uboot/u-boot-* CROSS_COMPILE=/home/cvondrachek/ti-sdk-am335x-evm-07.00.00.00/linux-devkit/sysroots/i686-arago-linux/usr/bin/arm-linux-gnueabihf- all
+make -C u-boot-* CROSS_COMPILE=$HOME/ti-sdk-am335x-evm-07.00.00.00/linux-devkit/sysroots/i686-arago-linux/usr/bin/arm-linux-gnueabihf- all
--- cleanNconfigUboot.sh
+++ cleanNconfigUboot.sh
@@ -1,2 +1,2 @@
-make -C /home/cvondrachek/ti-sdk-am335x-evm-07.00.00.00/uboot/u-boot-* CROSS_COMPILE=/home/cvondrachek/ti-sdk-am335x-evm-07.00.00.00/linux-devkit/sysroots/i686-arago-linux/usr/bin/arm-linux-gnueabihf-  distclean
-make -C /home/cvondrachek/ti-sdk-am335x-evm-07.00.00.00/uboot/u-boot-* CROSS_COMPILE=/home/cvondrachek/ti-sdk-am335x-evm-07.00.00.00/linux-devkit/sysroots/i686-arago-linux/usr/bin/arm-linux-gnueabihf-  am335x_bonelogic_config
+make -C u-boot-* CROSS_COMPILE=$HOME/ti-sdk-am335x-evm-07.00.00.00/linux-devkit/sysroots/i686-arago-linux/usr/bin/arm-linux-gnueabihf-  distclean
+make -C u-boot-* CROSS_COMPILE=$HOME/ti-sdk-am335x-evm-07.00.00.00/linux-devkit/sysroots/i686-arago-linux/usr/bin/arm-linux-gnueabihf-  am335x_bonelogic_config
